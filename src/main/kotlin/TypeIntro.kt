ab32346a448b6fbf72c4ee327550342c2e58bacc
cash <cashwugeek@icloud.com>
2026-02-05 10:55:21 +0800

    ch 02

---
+++ src/main/kotlin/TypeIntro.kt
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author cash.wu 
+ * @since 2026/02/05 
+ * 
+ */
+
+const val MAX_EXPERIENCE : Int = 5000
+
+fun main(args: Array<String>) {
+    val playerName = "cash"
+    var experiencePoints = 5
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+}
