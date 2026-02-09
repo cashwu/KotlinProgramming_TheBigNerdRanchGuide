9f9b020b9f2ba0d3bee1f1492c1af7dd9dac9b34
cash <cashwugeek@icloud.com>
2026-02-09 22:38:33 +0800

    Ch 04-7

--- NyetHack/src/main/kotlin/Game.kt
+++ NyetHack/src/main/kotlin/Game.kt
@@ -41,22 +41,18 @@ private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean
     return auraColor
 }
 
-private fun castFireball(numFireballs: Int = 2) {
+private fun castFireball(numFireballs: Int = 2) =
     println("A glass of Fireball springs into existence. (x$numFireballs)")
-}
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
-    val healthStatus = when (healthPoints) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) {
-            "has some minor wounds but is healing quite quickly!"
-        } else {
-            "has some minor wounds."
-        }
-
-        in 15..74 -> "has a few scratches."
-        else -> "is in awful condition!"
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String = when (healthPoints) {
+    100 -> "is in excellent condition!"
+    in 90..99 -> "has a few scratches."
+    in 75..89 -> if (isBlessed) {
+        "has some minor wounds but is healing quite quickly!"
+    } else {
+        "has some minor wounds."
     }
-    return healthStatus
+
+    in 15..74 -> "has a few scratches."
+    else -> "is in awful condition!"
 }
