bc928d1f4240c53fb9ba4d26271c199bef66b771
cash <cashwugeek@icloud.com>
2026-02-10 22:43:42 +0800

    Ch 06-4.3

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -9,7 +9,15 @@ fun main() {
 //        }
 //    }
 
-    var beverage = readLine()!!.capitalize()
+//    var beverage = readLine()!!.capitalize()
+
+    var beverage = readLine()
+
+    if (beverage != null) {
+        beverage = beverage.capitalize()
+    } else {
+        println("I can't do that without crashing - beverage was null!")
+    }
 
 //    beverage = null
 
