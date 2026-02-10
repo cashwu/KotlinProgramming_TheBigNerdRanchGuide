7462842f1c7b407a4b52192a84e1b362e7e5f8a0
cash <cashwugeek@icloud.com>
2026-02-10 22:09:11 +0800

    Ch 05-5

--- Sanbox/src/main/kotlin/SimVillage.kt
+++ Sanbox/src/main/kotlin/SimVillage.kt
@@ -36,16 +36,25 @@ fun main() {
 //    runSimulation("Guyal", greetingFunction)
 
 
-    runSimulation("Guyal") { playerName: String, numBuildings: Int ->
+    runSimulation("Guyal", ::printConstructionCost) { playerName: String, numBuildings: Int ->
         val currentYear = 2018
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
 }
 
-inline fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+inline fun runSimulation(
+    playerName: String,
+    costPrinter: (Int) -> Unit,
+    greetingFunction: (String, Int) -> String
+) {
 
     val numBuildings = (1..3).shuffled().last()
-
+    costPrinter(numBuildings)
     println(greetingFunction(playerName, numBuildings))
+}
+
+fun printConstructionCost(numBuildings: Int) {
+    val cost = 500
+    println("construction cost: ${cost * numBuildings}")
 }
