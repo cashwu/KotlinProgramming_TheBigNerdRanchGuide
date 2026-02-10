595736b526c0ba0c4089d6ee015ffaa6996f3823
cash <cashwugeek@icloud.com>
2026-02-10 22:49:07 +0800

    Ch 06-5

---
+++ NyetHack/src/main/kotlin/SwordJuggler.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    var swordsJuggling: Int? = null
+
+    val isJugglingProficient = (1..3).shuffled().last() == 3
+
+    if (isJugglingProficient) {
+        swordsJuggling = 2
+    }
+
+    swordsJuggling = swordsJuggling!!.plus(1)
+    println("You juggle $swordsJuggling swords!")
+}
