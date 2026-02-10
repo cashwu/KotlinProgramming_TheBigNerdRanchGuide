44f336a4fc2ff26f25f10c8101cad9882da91127
cash <cashwugeek@icloud.com>
2026-02-10 22:13:50 +0800

    Ch 05-6

--- Sanbox/src/main/kotlin/SimVillage.kt
+++ Sanbox/src/main/kotlin/SimVillage.kt
@@ -36,23 +36,43 @@ fun main() {
 //    runSimulation("Guyal", greetingFunction)
 
 
-    runSimulation("Guyal", ::printConstructionCost) { playerName: String, numBuildings: Int ->
+//    runSimulation("Guyal", ::printConstructionCost) { playerName: String, numBuildings: Int ->
+//        val currentYear = 2018
+//        println("Adding $numBuildings houses")
+//        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+//    }
+
+    runSimulation()
+}
+
+fun runSimulation() {
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("Guyal"))
+}
+
+fun configureGreetingFunction(): (String) -> String {
+
+    val structureType = "hospitals"
+    var numBuildings = 5
+
+    return { playerName: String ->
         val currentYear = 2018
-        println("Adding $numBuildings houses")
+        numBuildings += 1
+        println("Adding $numBuildings $structureType")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
 }
 
-inline fun runSimulation(
-    playerName: String,
-    costPrinter: (Int) -> Unit,
-    greetingFunction: (String, Int) -> String
-) {
-
-    val numBuildings = (1..3).shuffled().last()
-    costPrinter(numBuildings)
-    println(greetingFunction(playerName, numBuildings))
-}
+//inline fun runSimulation(
+//    playerName: String,
+//    costPrinter: (Int) -> Unit,
+//    greetingFunction: (String, Int) -> String
+//) {
+//
+//    val numBuildings = (1..3).shuffled().last()
+//    costPrinter(numBuildings)
+//    println(greetingFunction(playerName, numBuildings))
+//}
 
 fun printConstructionCost(numBuildings: Int) {
     val cost = 500
