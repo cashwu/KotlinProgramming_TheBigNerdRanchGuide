ace44cdff706af8b500bc6f8f273b9655e677165
cash <cashwugeek@icloud.com>
2026-02-12 20:56:15 +0800

    Ch 07-1.2

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -2,13 +2,20 @@ const val TAVERN_NAME = "Taernyl's Folly"
 
 fun main() {
 
-    placeOrder()
+    placeOrder("shandy,Dragon's Breath,5.91")
 }
 
-fun placeOrder() {
+fun placeOrder(menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
-
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
-
     println("Madrigal speaks with $tavernMaster about their order.")
+
+//    val data = menuData.split(",")
+//    val type = data[0]
+//    val name = data[1]
+//    val price = data[2]
+
+    val (type, name, price) = menuData.split(",")
+    val message = "Madrigal buys a $name ($type) for $price"
+    println(message)
 }
