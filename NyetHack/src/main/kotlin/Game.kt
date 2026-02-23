fdb011655eda2e2d030753906741ffc197907cdc
cash <cashwugeek@icloud.com>
2026-02-23 10:31:32 +0800

    Ch 12-3

--- NyetHack/src/main/kotlin/Game.kt
+++ NyetHack/src/main/kotlin/Game.kt
@@ -5,6 +5,7 @@
  * 
  */
 
+val player = Player()
 fun main() {
     val name = "Madrigal"
     var healthPoints = 89
@@ -12,8 +13,7 @@ fun main() {
     val isImmortal = false
 
     val player = Player()
-
-
+    player.castFireball()
 
     // aura
     val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
@@ -23,7 +23,7 @@ fun main() {
     printPlayerStatus(auraColor, isBlessed, name, healthStatus)
 
 //    castFireball(5)
-    castFireball()
+//    castFireball()
 }
 
 private fun printPlayerStatus(
@@ -45,8 +45,8 @@ private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean
     return auraColor
 }
 
-private fun castFireball(numFireballs: Int = 2) =
-    println("A glass of Fireball springs into existence. (x$numFireballs)")
+//private fun castFireball(numFireballs: Int = 2) =
+//    println("A glass of Fireball springs into existence. (x$numFireballs)")
 
 private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String = when (healthPoints) {
     100 -> "is in excellent condition!"
--- NyetHack/src/main/kotlin/Player.kt
+++ NyetHack/src/main/kotlin/Player.kt
@@ -5,4 +5,7 @@
  * 
  */
 class Player {
+
+    fun castFireball(numFireballs: Int = 2) = println("A glass of Fireball springs into existence. (x$numFireballs)")
+
 }
