df5b81268f2254a9531bf52c0f29bcce84a83267
cash <cashwugeek@icloud.com>
2026-02-24 22:47:56 +0800

    Ch 16-3

--- NyetHack/src/main/kotlin/com/bignerdranch/nyethack/Creature.kt
+++ NyetHack/src/main/kotlin/com/bignerdranch/nyethack/Creature.kt
@@ -1,10 +1,15 @@
 package com.bignerdranch.nyethack
 
+import kotlin.random.Random
+
 interface Fightable {
     var healthPoints: Int
     val diceCount : Int
     val diceSides: Int
     val damageRoll: Int
+        get() = (0 until diceCount).sumOf {
+            Random.nextInt(diceSides) + 1
+        }
 
     fun attack(opponent: Fightable) : Int
 }
