29319fe776d81c629e79f0c512ee80b24956cda2
cash <cashwugeek@icloud.com>
2026-02-25 10:04:06 +0800

    Ch 17-2

--- Sanbox/src/main/kotlin/Generics.kt
+++ Sanbox/src/main/kotlin/Generics.kt
@@ -7,7 +7,13 @@
 
 class LootBox<T>(item: T) {
 
+    var open = false
+
     private var loot: T = item
+
+    fun fetch(): T? {
+        return loot.takeIf { open }
+    }
 }
 
 class Fedora(val name: String, val value: Int)
@@ -18,4 +24,14 @@ fun main() {
     val lootBoxOne = LootBox(Fedora("cash", 100))
 
     val lootBoxTwo = LootBox(Coin(100))
+
+    lootBoxOne.open = true
+
+    lootBoxOne.fetch()?.let {
+        println("let - you retrieve ${it.name} from the box")
+    }
+
+    lootBoxOne.fetch()?.run {
+        println("run - you retrieve $name from the box")
+    }
 }
