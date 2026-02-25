1e7b9bb6587c44cf25f6da18027183b6394addb0
cash <cashwugeek@icloud.com>
2026-02-25 10:14:35 +0800

    Ch 17-5

--- Sanbox/src/main/kotlin/Generics.kt
+++ Sanbox/src/main/kotlin/Generics.kt
@@ -5,18 +5,20 @@
  * 
  */
 
-class LootBox<T : Loot>(item: T) {
+class LootBox<T : Loot>(vararg item: T) {
 
     var open = false
 
-    private var loot: T = item
+    private var loot: Array<out T> = item
 
-    fun fetch(): T? {
-        return loot.takeIf { open }
+    operator fun get(index: Int): T? = loot[index].takeIf { open }
+
+    fun fetch(item: Int): T? {
+        return loot[item].takeIf { open }
     }
 
-    fun <R> fetch(lootModFunction: (T) -> R): R? {
-        return lootModFunction(loot).takeIf { open }
+    fun <R> fetch(item: Int, lootModFunction: (T) -> R): R? {
+        return lootModFunction(loot[item]).takeIf { open }
     }
 }
 
@@ -26,25 +28,33 @@ class Coin(value: Int) : Loot(value)
 
 fun main() {
 
-    val lootBoxOne = LootBox(Fedora("cash", 100))
+    val lootBoxOne = LootBox(
+        Fedora("a generic-looking magenta fedora", 15),
+        Fedora("a dazzling magenta fedora", 25)
+    )
 
     val lootBoxTwo = LootBox(Coin(100))
 
     lootBoxOne.open = true
 
-    lootBoxOne.fetch()?.let {
+    lootBoxOne.fetch(1)?.let {
         println("let - you retrieve ${it.name} from the box")
     }
 
-    lootBoxOne.fetch()?.run {
+    lootBoxOne.fetch(1)?.run {
         println("run - you retrieve $name from the box")
     }
 
-    val coin = lootBoxOne.fetch() {
+    val coin = lootBoxOne.fetch(0) {
         Coin(it.value * 3)
     }
 
     coin?.let {
         println(it.value)
+    }
+
+    val fedora = lootBoxOne[1]
+    fedora?.let {
+        println(it.name)
     }
 }
