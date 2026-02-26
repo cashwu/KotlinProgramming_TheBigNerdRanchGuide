53d5d16f9b8d28fef612c36a72ecbc72758262b3
cash <cashwugeek@icloud.com>
2026-02-26 22:10:38 +0800

    Ch 18-2

--- Sanbox/src/main/kotlin/Extensions.kt
+++ Sanbox/src/main/kotlin/Extensions.kt
@@ -1,11 +1,20 @@
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
-fun Any.easyPrint() = println(this)
+//fun Any.easyPrint() : Any {
+//    println(this)
+//    return this
+//}
 
+fun <T> T.easyPrint(): T {
+    println(this)
+    return this
+}
 
 fun main() {
-    "Madrigal has left the building".addEnthusiasm().easyPrint()
+    "Madrigal has left the building"
+        .easyPrint()
+        .addEnthusiasm()
+        .easyPrint()
 
     42.easyPrint()
-
 }
